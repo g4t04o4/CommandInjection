3c88b232a86907ef8df275c9a1ee4ea88d78b8b0
g4t0 <repmah@gmail.com>
2019-07-08 20:00:10 +0300

    localisation

--- pingcheck/first_one/models.py
+++ pingcheck/first_one/models.py
@@ -1,1 +1,9 @@
 # Create your models here.
+from django.db import models
+
+
+class BlogPost(models.Model):
+    name = models.CharField(max_length=30)
+    text = models.CharField(max_length=2562)
+    image = models.FilePathField()
+    pub_date = models.DateTimeField()
--- pingcheck/first_one/views.py
+++ pingcheck/first_one/views.py
@@ -1,6 +1,5 @@
 import subprocess
 
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -9,10 +8,8 @@ from django.shortcuts import render
 def ping(request):
     if request.method == 'POST':
         ipaddress = request.POST['ipaddress']
-
-        result = subprocess.Popen('ping -w 5 ' + ipaddress, shell=True, stdout=subprocess.PIPE).stdout.read()
-
-        # return HttpResponse('<pre>' + result.decode('cp866') + '</pre>')
+        result = subprocess.Popen('ping -w 5 ' + ipaddress, shell=True,
+                                  stdout=subprocess.PIPE).stdout.read()
         return render(request, 'ping_result.html', {'result': result.decode('cp866')})
     else:
-        return render(request, 'ping_input.html')
+        return render(request, 'all_posts.html')
